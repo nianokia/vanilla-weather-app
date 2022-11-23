6a4d20c5b80f489e1582588439ccf3dc0bbd5915
nianokia <111546789+nianokia@users.noreply.github.com>
2022-11-22 22:35:09 -0500

    Added JS Current Time

--- src/app.js
+++ src/app.js
@@ -1,7 +1,47 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = [
+    12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11,
+  ];
+  let hour = hours[date.getHours()];
+  let amPmSpread = [
+    "am",
+    "am",
+    "am",
+    "am",
+    "am",
+    "am",
+    "am",
+    "am",
+    "am",
+    "am",
+    "am",
+    "am",
+    "pm",
+    "pm",
+    "pm",
+    "pm",
+    "pm",
+    "pm",
+    "pm",
+    "pm",
+    "pm",
+    "pm",
+    "pm",
+    "pm",
+  ];
+  let amPm = amPmSpread[date.getHours()];
+  return `${hour} ${amPm}`;
+}
+
 function displayTemp(response) {
   console.log(response.data);
   let cityName = document.querySelector("#city-name");
   cityName.innerHTML = response.data.city;
+
+  let date = document.querySelector("#current-time");
+  date.innerHTML = formatDate(response.data.time * 1000);
 
   let temp = Math.round(response.data.temperature.current);
   let currentTemperature = document.querySelector("#temp");
