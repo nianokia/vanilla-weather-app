9b9368da58569cd90b9c639b36275b6c88dc3592
nianokia <111546789+nianokia@users.noreply.github.com>
2022-11-22 22:14:36 -0500

    Added HTML Weather Descriptions + JS Weather Conditions

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 function displayTemp(response) {
   console.log(response.data);
-  let cityName = response.data.city;
-  let city = document.querySelector("#city-name");
-  city.innerHTML = cityName;
-  let currentTemp = Math.round(response.data.temperature.current);
-  let temperature = document.querySelector("#temp");
-  temperature.innerHTML = currentTemp;
+  let cityName = document.querySelector("#city-name");
+  cityName.innerHTML = response.data.city;
+
+  let temp = Math.round(response.data.temperature.current);
+  let currentTemperature = document.querySelector("#temp");
+  currentTemperature.innerHTML = temp;
+
+  let condition = document.querySelector("#current-condition");
+  condition.innerHTML = response.data.condition.description;
+
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = `${response.data.wind.speed} mph`;
 }
 
 let apiKey = "9733a0bfob8d3b90ab42ae5a571ftfa4";
